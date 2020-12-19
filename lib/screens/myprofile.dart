59698215a14b8cf926d604a203686830ba0aabdc
Deepanjali Tripathi <68412187+Dia96@users.noreply.github.com>
2020-12-19 05:08:46 -0800

    remove unused code

--- lib/screens/myprofile.dart
+++ lib/screens/myprofile.dart
@@ -13,127 +13,54 @@ class MyProfile extends StatefulWidget {
   @override
   _MyProfileState createState() => _MyProfileState();
 }
-
 enum Gender{Male, Female, Trans, Prefer_not_to_say}
-
 class _MyProfileState extends State<MyProfile> {
-
   Gender _selected = Gender.Male;
   String _value1 = '';
   List<PopupMenuEntry<Gender>> _items = new List<PopupMenuEntry<Gender>>();
 
-
-  void initState() {
-    for(Gender gender in Gender.values) {
-      _items.add(new PopupMenuItem(
-        child: new Text(_getDisplay(gender),),
-        value: gender,
-      ));
-    }
-  }
-
-  void _onSelected(Gender gender) {
-    setState((){
-      _selected = gender;
-      _value1 = '${_getDisplay(gender)}';
+  void initState() {for(Gender gender in Gender.values) {
+      _items.add(new PopupMenuItem(child: new Text(_getDisplay(gender)),
+        value: gender));
+    }}
+    void _onSelected(Gender gender) {
+    setState((){_selected = gender;_value1 = '${_getDisplay(gender)}';
     });
   }
-
-  String _getDisplay(Gender gender) {
-    int index = gender.toString().indexOf('.');
-    index++;
-    return gender.toString().substring(index);
+  String _getDisplay(Gender gender) {int index = gender.toString().indexOf('.');
+    index++; return gender.toString().substring(index);
   }
-
-
-
-
   String _value = '';
-
-  Future _selectDate() async {
-    DateTime picked = await showDatePicker(
-        context: context,
-        initialDate: new DateTime.now(),
-        firstDate: new DateTime(1950),
-        lastDate: new DateTime(2099)
-    );
+  Future _selectDate() async {DateTime picked = await showDatePicker(
+      context: context, initialDate: new DateTime.now(), firstDate: new DateTime(1950), lastDate: new DateTime(2099));
     if(picked != null) setState(() => _value = picked.toString());
   }
-
-
-
   @override
-  Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () {
-        Navigator.pushReplacement(context,
-            new MaterialPageRoute(builder: (context) => HomeScreen()));
+  Widget build(BuildContext context) {return WillPopScope(onWillPop: () {
+    Navigator.pushReplacement(context, new MaterialPageRoute(builder: (context) => HomeScreen()));
       },
-      child: Scaffold(
-        backgroundColor: SecondaryColor,
-        body: CustomScrollView(
-          slivers: [
-            SliverAppBar(
-              leading: Builder( builder: (BuildContext context) {
-                return IconButton(
-                  icon: const Icon(Icons.arrow_back),
-                  onPressed: () {
-                    Navigator.pushReplacement(context,
-                        new MaterialPageRoute(builder: (context) => HomeScreen()));
-                  },
-                );
-              },),
-              backgroundColor: PrimaryColor,
-              expandedHeight: 300,
-              pinned: true,
-              floating: true,
-              flexibleSpace: FlexibleSpaceBar(
-                title: Text('My Profile',),
-                background: Image.network(photoUrl, fit: BoxFit.fill))),
-            SliverList(delegate: SliverChildListDelegate(<Widget>[
-              SizedBox(height: 50),
-
-              Slidable(
-                actionPane: SlidableDrawerActionPane(),
-                actionExtentRatio: 0.25,
-                child: Container(
-                    height: 60,
-                    margin: EdgeInsets.only(left: 20, right: 100),
-                    decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.cyan[700],
-                          blurRadius: 40.0,
-                          spreadRadius: 1.0,
-                          offset: Offset(
-                            2.0,
-                            2.0,
-                          ),
-                        )
-                      ],
-                      borderRadius: BorderRadius.all(Radius.circular(100.0),),
-                      gradient: LinearGradient(colors: <Color>[
-                        Colors.cyan[200],
-                        Colors.cyan[400]
-                      ]),
-                    ),
-                    child: ListTile(trailing: Icon(Icons.slideshow),
-                      leading: Icon(FontAwesomeIcons.slidersH),
-                      title: Text('User Id',style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold ),
-                        textAlign: TextAlign.center,),
-                      subtitle: Text(name
-                          , style: TextStyle(fontSize: 15.0), textAlign: TextAlign.center),
-                    )),
-
-                secondaryActions: <Widget>[
-                  IconSlideAction(
-                    caption: 'Edit',
-                    color: PrimaryColor,
-                    icon: Icons.mode_edit,
-                    onTap: () {},
-                  ),
-                ],
-              ),
+    child: Scaffold(backgroundColor: SecondaryColor, body: CustomScrollView(
+      slivers: [SliverAppBar(leading: Builder( builder: (BuildContext context) {
+        return IconButton(icon: const Icon(Icons.arrow_back),
+          onPressed: () {Navigator.pushReplacement(context, new MaterialPageRoute(builder: (context) => HomeScreen()));
+        });
+      }),
+          backgroundColor: PrimaryColor, expandedHeight: 300, pinned: true, floating: true,
+          flexibleSpace: FlexibleSpaceBar(title: Text('My Profile'), background: Image.network(photoUrl, fit: BoxFit.fill))),
+        SliverList(delegate: SliverChildListDelegate(<Widget>[
+          SizedBox(height: 50),
+          Slidable(actionPane: SlidableDrawerActionPane(), actionExtentRatio: 0.25,
+            child: Container(height: 60, margin: EdgeInsets.only(left: 20, right: 100),
+                decoration: BoxDecoration(boxShadow: [
+                  BoxShadow(color: Colors.cyan[700], blurRadius: 40.0, spreadRadius: 1.0, offset: Offset(2.0, 2.0))],
+                  borderRadius: BorderRadius.all(Radius.circular(100.0)),
+                    gradient: LinearGradient(colors: <Color>[Colors.cyan[200], Colors.cyan[400]])),
+                child: ListTile(trailing: Icon(Icons.slideshow), leading: Icon(FontAwesomeIcons.slidersH),
+                  title: Text('User Id',style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold ), textAlign: TextAlign.center),
+                  subtitle: Text(name, style: TextStyle(fontSize: 15.0), textAlign: TextAlign.center))),
+            secondaryActions: <Widget>[IconSlideAction(caption: 'Edit', color: PrimaryColor, icon: Icons.mode_edit,
+              onTap: () {},
+            )]),
 
               SizedBox(height: 30),
 
